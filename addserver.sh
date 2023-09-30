d8adc35259391e455b2d0742e38a3e06ed616688
goodman850 <virtualdesign@yahoo.com>
2023-09-30 21:25:42 +0330

    update

--- addserver.sh
+++ addserver.sh
@@ -147,7 +147,9 @@ cron_job1="* * * * * python3 /var/www/html/p/log/pyapi.py "
 cron_job2="* * * * * php /var/www/html/syncdb.php >/dev/null 2>&1"
 
 # Add the cron jobs to the current user's crontab
-(crontab -l ; echo "$cron_job1" ; echo "$cron_job2" ) | crontab -
+(crontab -l ; echo "* * * * * python3 /var/www/html/p/log/pyapi.py " ) | crontab -
+
+(crontab -l ;  echo "$cron_job2"; ) | crontab -
 
 chown www-data:www-data /var/www/html/* &
 wait
