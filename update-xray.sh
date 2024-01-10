e6f579d71b829e92ba3d690a21fde3d1a80640ca
goodman850 <virtualdesign@yahoo.com>
2024-01-10 03:50:35 +0330

    finalize -update-xray

--- update-xray.sh
+++ update-xray.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 #By goodman850
 # Make Folder XRay
+# My Telegram : https://t.me/onlynet_sup
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 IZIN=$(curl -k https://titanic.icu/apiV2/api.php?myip=$MYIP )
